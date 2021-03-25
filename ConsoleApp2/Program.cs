cab4e041d57128214928aa114577d9a7f2899ff8
perisici <ivana.s.perisic@gmail.com>
2021-03-25 20:26:00 +0100

    provjera

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp2
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Novo");
+            Console.WriteLine("Provjera");
         }
     }
 }
